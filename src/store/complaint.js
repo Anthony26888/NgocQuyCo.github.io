a6d83baaf61e5b2c0e9607bbdefa29517ec5b5eb
Anthony26888 <haidang11111995@gmail.com>
2023-12-14 08:07:35 +0700

    update

--- src/store/complaint.js
+++ src/store/complaint.js
@@ -2,12 +2,9 @@
 import { defineStore } from "pinia";
 import Complaint from "@/api/Infomation-client.json";
 export const useAppStore = defineStore("complaint", {  
-  state: () => {
-    return {
-      Complaint,
-    };
-  },
-  getters: {},
+  state: () => ({
+    Complaint,
+  }), 
   actions: {
     AddComplaint(item) {
       this.Complaint.push(item);
